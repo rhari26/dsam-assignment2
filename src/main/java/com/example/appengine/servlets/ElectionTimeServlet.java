89627a5daad1ba87a073a7781cf10fe719cd3542
Hari Prashanth Rajendran <rhari26@gmail.com>
2019-01-23 00:24:16 +0100

    Election Date and Time management done

---
+++ src/main/java/com/example/appengine/servlets/ElectionTimeServlet.java
@@ -0,0 +1,63 @@
+package com.example.appengine.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
+/**
+ * Servlet implementation class ElectionTimeServlet
+ */
+//@WebServlet("/ElectionTimeServlet")
+public class ElectionTimeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ElectionTimeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/elctionTime.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String electionDate = request.getParameter("date");
+		String electionStartTime = request.getParameter("startTime");
+		String electionEndTime = request.getParameter("endTime");
+		
+		try {
+			DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+			Entity election = new Entity("Elections");
+			
+			election.setProperty("electionDate", electionDate);
+			election.setProperty("electionStartTime", electionStartTime);
+			election.setProperty("electionEndTime", electionEndTime);
+			
+			ds.put(election);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		response.sendRedirect("http://localhost:8080/election-time ");
+	}
+
+}
---
+++ src/main/webapp/electionTime.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.*" %>
+<%@ page import="com.example.appengine.java8.ElectionSummary" %>
+<%@ page import="com.google.appengine.api.datastore.Entity" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Election Time</title>
+</head>
+<body>
+<h2>Manage Election Time/Date</h2>
+<% ElectionSummary es = new ElectionSummary();
+  List<Entity> electionTime = es.candidateList();
+  if(electionTime.isEmpty()){
+   %>
+   <form action="/election-time" method="post" id="voter-form">
+	<div id="date-text">
+		<input type="date" name="date" id="date" placeholder="Election Date" required="required"><br>
+	</div>
+	<div id="startTime-text">
+		<input type="time" name="startTime" id="startTime" placeholder="Election Start Time" required="required"><br>
+	</div>
+	<div id="endTime-text">
+		<input type="time" name="endTime" id="endTime" placeholder="Election End Time" required="required"><br>
+	</div>
+	<input type="submit" value="Submit" name="submit">
+</form>
+ <% } else { %>
+ 	<% for(Entity election : electionTime){  %>
+ 	<p>Election Date: <%= election.getProperty("electionDate") %></p>
+ 	<p>Starting at: <%= election.getProperty("electionStartTime") %> till <%= election.getProperty("electionEndTime") %></p>
+ <% } } %>
+</body>
+</html>
